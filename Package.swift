09dd1d6e6ef6d83f805e1b9e65085bfcf3040379
dankinsoid <voidilov@gmail.com>
2022-04-29 17:21:56 +0300

    hm

--- Package.swift
+++ Package.swift
@@ -51,7 +51,7 @@ let package = Package(
       .package(url: "https://github.com/dankinsoid/VDCodable", from: "2.10.0"),
 			.package(url: "https://github.com/Kitura/swift-html-entities.git", from: "3.0.0"),
 			.package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.4.0"),
-			.package(url: "https://github.com/Kuniwak/MultipartFormDataKit.git", from: "1.0.1"),
+			.package(url: "https://github.com/dankinsoid/MultipartFormDataKit.git", from: "1.0.2"),
 			.package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0")
     ],
     targets: [
