02c3c05dd05ff2f28cc8c2361ec7f89e2b230d7b
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2022-10-28 17:32:49 +0700

    1.1.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,104 +6,105 @@ import PackageDescription
 let package = Package(
     name: "SwiftMusicServicesAPI",
     platforms: [
-      .macOS("10.15"),
-      .iOS("13.0"),
-      .tvOS("13.0"),
-      .watchOS("6.0")
+        .macOS("10.15"),
+        .iOS("13.0"),
+        .tvOS("13.0"),
+        .watchOS("6.0")
     ],
     products: [
-      .library(
-          name: "SpotifyAPI",
-          targets: ["SpotifyAPI"]
-      ),
-			.library(
-				name: "SpotifyLogin",
-				targets: ["SpotifyLogin"]
-			),
-      .library(
-          name: "YandexMusicAPI",
-          targets: ["YandexMusicAPI"]
-      ),
-			.library(
-				name: "YandexMusicLogin",
-				targets: ["YandexMusicLogin"]
-			),
-			.library(
-					name: "VKMusicAPI",
-					targets: ["VKMusicAPI"]
-			),
-			.library(
-					name: "VKLogin",
-					targets: ["VKLogin"]
-			),
-			.library(
-					name: "AppleMusicAPI",
-					targets: ["AppleMusicAPI"]
-			),
-			.library(
-					name: "AppleMusicLogin",
-					targets: ["AppleMusicLogin"]
-			),
-		],
+        .library(
+            name: "SpotifyAPI",
+            targets: ["SpotifyAPI"]
+        ),
+        .library(
+            name: "SpotifyLogin",
+            targets: ["SpotifyLogin"]
+        ),
+        .library(
+            name: "YandexMusicAPI",
+            targets: ["YandexMusicAPI"]
+        ),
+        .library(
+            name: "YandexMusicLogin",
+            targets: ["YandexMusicLogin"]
+        ),
+        .library(
+            name: "VKMusicAPI",
+            targets: ["VKMusicAPI"]
+        ),
+        .library(
+            name: "VKLogin",
+            targets: ["VKLogin"]
+        ),
+        .library(
+            name: "AppleMusicAPI",
+            targets: ["AppleMusicAPI"]
+        ),
+        .library(
+            name: "AppleMusicLogin",
+            targets: ["AppleMusicLogin"]
+        ),
+    ],
     dependencies: [
-      .package(url: "https://github.com/dankinsoid/swift-http", from: "1.2.2"),
-      .package(url: "https://github.com/dankinsoid/VDCodable", from: "2.10.0"),
-			.package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.4.0"),
-			.package(url: "https://github.com/dankinsoid/MultipartFormDataKit.git", from: "1.0.2"),
-			.package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0")
+        .package(url: "https://github.com/BinaryBirds/swift-http.git", from: "1.1.0"),
+        .package(url: "https://github.com/dankinsoid/VDCodable", from: "2.10.0"),
+        .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.4.0"),
+        .package(url: "https://github.com/dankinsoid/MultipartFormDataKit.git", from: "1.0.2"),
+        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0")
     ],
     targets: [
-      .target(
-          name: "SwiftMusicServicesApi",
-          dependencies: [
-            .product(name: "SwiftHttp", package: "swift-http"),
-            .product(name: "VDCodable", package: "VDCodable")
-          ]
-      ),
-      .target(
-          name: "SpotifyAPI",
-          dependencies: [.target(name: "SwiftMusicServicesApi")]
-      ),
-      .target(
-          name: "SpotifyLogin",
-          dependencies: []
-      ),
-      .target(
-          name: "YandexMusicAPI",
-          dependencies: [.target(name: "SwiftMusicServicesApi")]
-      ),
-			.target(
-				name: "YandexMusicLogin",
-				dependencies: [.target(name: "YandexMusicAPI")]
-			),
-			.target(
-					name: "VKMusicAPI",
-					dependencies: [
-						.target(name: "SwiftMusicServicesApi"),
-						"SwiftSoup",
-						"MultipartFormDataKit"
-					]
-			),
-			.target(
-					name: "VKLogin",
-					dependencies: [.target(name: "VKMusicAPI")]
-			),
-      .testTarget(
-          name: "SwiftMusicServicesApiTests",
-          dependencies: ["SwiftMusicServicesApi"]
-      ),
-			.target(
-					name: "AppleMusicAPI",
-					dependencies: [
-						"SwiftMusicServicesApi"
-					]
-			),
-			.target(
-					name: "AppleMusicLogin",
-					dependencies: [
-						.target(name: "AppleMusicAPI"),
-						.product(name: "JWTKit", package: "jwt-kit")
-					]
-			),
+        .target(
+            name: "SwiftMusicServicesApi",
+            dependencies: [
+                .product(name: "SwiftHttp", package: "swift-http"),
+                .product(name: "VDCodable", package: "VDCodable")
+            ]
+        ),
+        .target(
+            name: "SpotifyAPI",
+            dependencies: [.target(name: "SwiftMusicServicesApi")]
+        ),
+        .target(
+            name: "SpotifyLogin",
+            dependencies: []
+        ),
+        .target(
+            name: "YandexMusicAPI",
+            dependencies: [.target(name: "SwiftMusicServicesApi")]
+        ),
+        .target(
+            name: "YandexMusicLogin",
+            dependencies: [.target(name: "YandexMusicAPI")]
+        ),
+        .target(
+            name: "VKMusicAPI",
+            dependencies: [
+                .target(name: "SwiftMusicServicesApi"),
+                "SwiftSoup",
+                "MultipartFormDataKit"
+            ]
+        ),
+        .target(
+            name: "VKLogin",
+            dependencies: [.target(name: "VKMusicAPI")]
+        ),
+        .testTarget(
+            name: "SwiftMusicServicesApiTests",
+            dependencies: ["SwiftMusicServicesApi"]
+        ),
+        .target(
+            name: "AppleMusicAPI",
+            dependencies: [
+                "SwiftMusicServicesApi"
+            ]
+        ),
+        .target(
+            name: "AppleMusicLogin",
+            dependencies: [
+                .target(name: "AppleMusicAPI"),
+                .product(name: "JWTKit", package: "jwt-kit")
+            ]
+        ),
     ]
 )
+
