a9c482232b11a6888962806d339d771e74423fba
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2024-04-10 15:25:52 +0300

    1.36.3

--- Package.swift
+++ Package.swift
@@ -48,7 +48,7 @@ let package = Package(
 	dependencies: [
 		.package(url: "https://github.com/BinaryBirds/swift-http.git", from: "1.2.2"),
 		.package(url: "https://github.com/dankinsoid/VDCodable", from: "2.13.0"),
-        .package(url: "https://github.com/dankinsoid/swift-api-client", from: "1.6.1"),
+        .package(url: "https://github.com/dankinsoid/swift-api-client", from: "1.6.2"),
 		.package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.4.0"),
 		.package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0"),
 	],
