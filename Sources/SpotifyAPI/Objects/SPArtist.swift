59745d2eab5fccfc3b3937562bda6522ae270b43
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2024-05-13 21:10:21 +0400

    1.38.17

--- Sources/SpotifyAPI/Objects/SPArtist.swift
+++ Sources/SpotifyAPI/Objects/SPArtist.swift
@@ -12,15 +12,15 @@ public struct SPArtist: Codable {
 	/// Images of the artist in various sizes, widest first.
 	public var images: [SPImage]?
 	/// The name of the artist.
-	public var name: String
+	public var name: String?
 	/// The popularity of the artist. The value will be between 0 and 100, with 100 being the most popular. The artist's popularity is calculated from the popularity of all the artist's tracks.
 	public var popularity: Int?
 	/// The object type: "artist"
 	public var type: String?
 	/// The [Spotify URI](/documentation/web-api/#spotify-uris-and-ids) for the artist.
-	public var uri: String
+	public var uri: String?
 
-	public init(externalUrls: SPExternalURL? = nil, followers: SPFollowers? = nil, genres: [String]? = nil, href: String? = nil, id: String, images: [SPImage]? = nil, name: String, popularity: Int? = nil, type: String? = nil, uri: String) {
+	public init(externalUrls: SPExternalURL? = nil, followers: SPFollowers? = nil, genres: [String]? = nil, href: String? = nil, id: String, images: [SPImage]? = nil, name: String? = nil, popularity: Int? = nil, type: String? = nil, uri: String? = nil) {
 		self.externalUrls = externalUrls
 		self.followers = followers
 		self.genres = genres
