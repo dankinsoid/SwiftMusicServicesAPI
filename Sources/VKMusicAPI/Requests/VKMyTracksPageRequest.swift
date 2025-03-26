75078e3ca9fc900556cf5100b27d489cecef6934
Danil <voidilov@gmail.com>
2025-03-26 16:01:58 +0300

    fix

--- Sources/VKMusicAPI/Requests/VKMyTracksPageRequest.swift
+++ Sources/VKMusicAPI/Requests/VKMyTracksPageRequest.swift
@@ -1,35 +1,34 @@
 import Foundation
-import SimpleCoders
 import SwiftAPIClient
 import SwiftSoup
 
 public extension VK.API {
 
-    func myTracksPageRequest(start_from: String?, block: String) async throws -> VKAudioListSection {
-        var string = try await client("audio")
-            .xmlHttpRequest
-            .query(MyTracksPageRequestInput(act: .block, block: block, start_from: start_from))
-            .bodyEncoder(.formURL)
-            .body(MyTracksPageRequestInputBody())
-            .post
-            .call(.http, as: .string)
-        string = string
-            .components(separatedBy: "\"playlist\"")
-            .dropFirst()
-            .joined(separator: "\"playlist\"")
-        string = "{\"data\":[{\"playlist\"\(string)"
-        let data = Data(string.utf8)
-        return try JSONDecoder().decode(MyTracksPageRequestInputOutput.self, from: data).playlist
-    }
+	func myTracksPageRequest(start_from: String?, block: String) async throws -> VKAudioListSection {
+		var string = try await client("audio")
+			.xmlHttpRequest
+			.query(MyTracksPageRequestInput(act: .block, block: block, start_from: start_from))
+			.bodyEncoder(.formURL)
+			.body(MyTracksPageRequestInputBody())
+			.post
+			.call(.http, as: .string)
+		string = string
+			.components(separatedBy: "\"playlist\"")
+			.dropFirst()
+			.joined(separator: "\"playlist\"")
+		string = "{\"data\":[{\"playlist\"\(string)"
+		let data = Data(string.utf8)
+		return try JSONDecoder().decode(MyTracksPageRequestInputOutput.self, from: data).playlist
+	}
 
-    func myTracksPageRequest(id: Int, start_from: String) async throws -> VKAudioListSection {
-        var string = try await client("audios\(id)")
-            .xmlHttpRequest
-            .query(MyTracksPageRequestInput(section: .my, start_from: start_from))
-            .bodyEncoder(.formURL)
-            .body(MyTracksPageRequestInputBody())
-            .post
-            .call(.http, as: .string)
+	func myTracksPageRequest(id: Int, start_from: String) async throws -> VKAudioListSection {
+		var string = try await client("audios\(id)")
+			.xmlHttpRequest
+			.query(MyTracksPageRequestInput(section: .my, start_from: start_from))
+			.bodyEncoder(.formURL)
+			.body(MyTracksPageRequestInputBody())
+			.post
+			.call(.http, as: .string)
 		string = string
 			.components(separatedBy: "\"playlist\"")
 			.dropFirst()
@@ -42,31 +41,31 @@ public extension VK.API {
 	struct MyTracksPageRequestInput: Codable {
 		public var act: VKAct?
 		public var start_from: String?
-        public var section: VKAudioPageInput.Section?
-        public var block: String?
-        
-        public init(act: VKAct? = nil, block: String? = nil, section:  VKAudioPageInput.Section? = nil, start_from: String?) {
-            self.act = act
-            self.block = block
-            self.section = section
-            self.start_from = start_from
-        }
+		public var section: VKAudioPageInput.Section?
+		public var block: String?
+
+		public init(act: VKAct? = nil, block: String? = nil, section: VKAudioPageInput.Section? = nil, start_from: String?) {
+			self.act = act
+			self.block = block
+			self.section = section
+			self.start_from = start_from
+		}
 	}
 
 	struct MyTracksPageRequestInputBody: Codable {
 		public var _ajax = 1
-        
-        public init(_ajax: Int = 1) {
-            self._ajax = _ajax
-        }
+
+		public init(_ajax: Int = 1) {
+			self._ajax = _ajax
+		}
 	}
 
 	struct MyTracksPageRequestInputOutput {
 		public var playlist: VKAudioListSection
-        
-        public init(playlist: VKAudioListSection) {
-            self.playlist = playlist
-        }
+
+		public init(playlist: VKAudioListSection) {
+			self.playlist = playlist
+		}
 	}
 }
 
@@ -79,9 +78,9 @@ extension VK.API.MyTracksPageRequestInputOutput: Decodable {
 			playlist = try keyed.decode(VKAudioListSection.self, forKey: "playlist")
 		} catch {
 			guard let playlists = try keyed.decode([VKAudioListSection].self, forKey: "playlists").first else {
-                throw DecodingError.dataCorrupted(
-                    DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Empty playlists")
-                )
+				throw DecodingError.dataCorrupted(
+					DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Empty playlists")
+				)
 			}
 			playlist = playlists
 		}
--- Sources/YandexMusicAPI/Requests/Tracks.swift
+++ Sources/YandexMusicAPI/Requests/Tracks.swift
@@ -1,24 +1,23 @@
 import Foundation
 import SwiftAPIClient
-import SimpleCoders
 
 public extension Yandex.Music.API {
 
 	func tracks(ids: [String], withPositions: Bool = true) async throws -> [YMO.Track] {
-        try await withThrowingTaskGroup(of: [YMO.Track].self, returning: [YMO.Track].self) { [self] group in
-            var i = 0
-            let maxSize = 100
-            while i < ids.count {
-                let chunk = Array(ids[i..<min(i + maxSize, ids.count)])
-                i += maxSize
-                group.addTask { [self] in
-                    try await client("tracks")
-                        .query(TracksInput(ids: chunk, withPositions: withPositions))
-                        .post()
-                }
-            }
-            return try await group.collect()
-        }
+		try await withThrowingTaskGroup(of: [YMO.Track].self, returning: [YMO.Track].self) { [self] group in
+			var i = 0
+			let maxSize = 100
+			while i < ids.count {
+				let chunk = Array(ids[i ..< min(i + maxSize, ids.count)])
+				i += maxSize
+				group.addTask { [self] in
+					try await client("tracks")
+						.query(TracksInput(ids: chunk, withPositions: withPositions))
+						.post()
+				}
+			}
+			return try await group.collect()
+		}
 	}
 
 	struct TracksInput: Encodable {
@@ -41,14 +40,14 @@ public extension Yandex.Music.API {
 public extension Yandex.Music.API {
 
 	func tracksDownloadInfo(id: String) async throws -> [YMO.DownloadInfo] {
-        try await client("tracks", id, "download-info").get()
+		try await client("tracks", id, "download-info").get()
 	}
 }
 
 extension Yandex.Music.API {
 
 	public func fileURL(xmlURL: URL, codec: YMO.Codec, trackId: String, uid: Int) async throws -> URL {
-        let xml = try await client.url(xmlURL).call(.http, as: .string)
+		let xml = try await client.url(xmlURL).call(.http, as: .string)
 		let host = try getXML(at: "host", xml: xml)
 		let path = try getXML(at: "path", xml: xml)
 		let ts = try getXML(at: "ts", xml: xml)
@@ -59,32 +58,32 @@ extension Yandex.Music.API {
 		let parameters = Parameters(trackId: trackId, uid: uid)
 		return try URLQueryEncoder().encode(parameters, for: base)
 	}
-    
-    public struct TrackIDs: AsyncSequence {
-    
-        public typealias Element = [YMO.Track]
-    
-        let ids: [String]
-        let withPositions: Bool
-        let api: YM.API
-        
-        public func makeAsyncIterator() -> AsyncIterator {
-            AsyncIterator(base: self)
-        }
-        
-        public func first() async throws -> [YMO.Track] {
-            var iterator = makeAsyncIterator()
-            return try await iterator.next() ?? []
-        }
-        
-        public struct AsyncIterator: AsyncIteratorProtocol {
-            let base: TrackIDs
-            
-            public mutating func next() async throws -> [YMO.Track]? {
-                nil
-            }
-        }
-    }
+
+	public struct TrackIDs: AsyncSequence {
+
+		public typealias Element = [YMO.Track]
+
+		let ids: [String]
+		let withPositions: Bool
+		let api: YM.API
+
+		public func makeAsyncIterator() -> AsyncIterator {
+			AsyncIterator(base: self)
+		}
+
+		public func first() async throws -> [YMO.Track] {
+			var iterator = makeAsyncIterator()
+			return try await iterator.next() ?? []
+		}
+
+		public struct AsyncIterator: AsyncIteratorProtocol {
+			let base: TrackIDs
+
+			public mutating func next() async throws -> [YMO.Track]? {
+				nil
+			}
+		}
+	}
 
 	private func getXML(at key: String, xml: String) throws -> String {
 		let all = xml.components(separatedBy: "<\(key)>")
@@ -94,9 +93,9 @@ extension Yandex.Music.API {
 		      .first
 		else {
 			throw DecodingError.keyNotFound(
-                PlainCodingKey(key),
-                DecodingError.Context(codingPath: [], debugDescription: "")
-            )
+				PlainCodingKey(key),
+				DecodingError.Context(codingPath: [], debugDescription: "")
+			)
 		}
 		return result
 	}
